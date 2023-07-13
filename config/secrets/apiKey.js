71da445edb9099fa2c8e705b2a180d6ab935f673
NyXXiR <lgh0334@gmail.com>
2023-07-13 17:15:25 +0900

    Update apiKey.js

--- config/secrets/apiKey.js
+++ config/secrets/apiKey.js
@@ -1,4 +1,4 @@
 module.exports = {
-  RIOT_API_KEY: "RGAPI-dcfebb88-1b5f-49fa-aed5-7b59bde505f3",
-  BLIZZARD_CLIENT_ID: "501218b0af234ae5998e6485b194d72d",
+  RIOT_API_KEY: "",
+  BLIZZARD_CLIENT_ID: "",
 };

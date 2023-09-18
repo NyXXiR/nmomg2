7f38d3b118fc6247cdf436a3c745339b8e0c923d
NyXXiR <lgh0334@gmail.com>
2023-09-18 16:38:00 +0900

    Update authController.js
    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,8 +11,7 @@ router.use(cors());
 
 /* 라이엇 RSO 로그인 관련 전역변수 */
 var clientID = "6e705d8e-d2da-4e14-943c-b41bc62e6499",
-  clientSecret =
-    "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiaHR0cHM6Ly9hdXRoLnJpb3RnYW1lcy5jb20iXSwiZXhwIjo0ODQ2MjQzNjY3LCJpYXQiOjE2OTA1NzAwNjcsImlzcyI6IjZlNzA1ZDhlLWQyZGEtNGUxNC05NDNjLWI0MWJjNjJlNjQ5OSIsImp0aSI6Im5GMk53WUJRVG9LMmE4RjJZOFQ3N1E9PSIsInN1YiI6IjZlNzA1ZDhlLWQyZGEtNGUxNC05NDNjLWI0MWJjNjJlNjQ5OSJ9.lpXW5Yvs1LJL7wTMBbcNdRqwiKTDk2EJdbe24CwESNr1-hWrza4h-G8_kIAANOL0kOpK8-KqZmEAAoIqXAWSaA";
+  clientSecret = "JmKBkL71SniKEE1Zka08yw==";
 
 var appBaseUrl = "https://nmomg.com",
   appCallbackUrl = appBaseUrl + "/auth/riot";
@@ -137,7 +136,7 @@ router.get("/riot", function (req, res) {
         // legibly print out our tokens
         res.send("<pre>" + JSON.stringify(tokens, false, 4) + "</pre>");
       } else {
-        res.send("/token request failed");
+        res.send("/token request failed" + tokens);
       }
     }
   );

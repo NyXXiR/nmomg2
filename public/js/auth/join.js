46b5ca154dea65fbf83e357ba29446bbe8516dd3
NyXXiR <lgh0334@gmail.com>
2023-09-18 14:42:22 +0900

    비밀번호 확인절차 개선
    .

--- public/js/auth/join.js
+++ public/js/auth/join.js
@@ -51,11 +51,36 @@ idInput.addEventListener("focusout", function () {
   });
 });
 
+// 비밀번호와 비밀번호 확인 필드의 값을 가져옵니다.
+const passwordInput = document.getElementById("pwInput");
+const repeatPasswordInput = document.getElementById("pwRepeatInput");
 
-/* 
-focusout 2개 더 만들어야함
-비밀번호 조건만들기
+// 결과를 표시할 요소를 가져옵니다.
+const passwordMatchMessage = document.getElementById("passwordMatchMessage");
 
-비밀번호확인 > 일치여부 확인
+// 비밀번호 확인 함수를 정의합니다.
+const checkPasswordMatch = () => {
+  // 비밀번호와 비밀번호 확인 필드의 값을 가져옵니다.
+  const password = passwordInput.value;
+  const repeatPassword = repeatPasswordInput.value;
 
-*/
+  // 비밀번호와 비밀번호 확인이 모두 입력되었을 때 비교합니다.
+  if (password && repeatPassword) {
+    if (password === repeatPassword) {
+      // 비밀번호가 일치하는 경우
+      passwordMatchMessage.textContent = "비밀번호가 일치합니다.";
+      passwordMatchMessage.style.color = "blue";
+    } else {
+      // 비밀번호가 일치하지 않는 경우
+      passwordMatchMessage.textContent = "비밀번호가 일치하지 않습니다.";
+      passwordMatchMessage.style.color = "red";
+    }
+  } else {
+    // 둘 중 하나라도 입력되지 않은 경우 메시지를 초기화합니다.
+    passwordMatchMessage.textContent = "";
+  }
+};
+
+// 입력 필드의 값이 변경될 때마다 checkPasswordMatch 함수 실행
+passwordInput.addEventListener("input", checkPasswordMatch);
+repeatPasswordInput.addEventListener("input", checkPasswordMatch);
